504e5815f11e924f7734a5938fa6a6c6b8961d20
wonsy1006 <wonsy1006@gmail.com>
2023-01-22 17:55:59 +0900

    notesController worked by myself

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,5 +1,6 @@
 const Note = require("../models/Note");
 const asyncHandler = require("express-async-handler");
+const { update } = require("../models/User");
 
 // @desc Get all notes
 // @route GET /notes
@@ -16,17 +17,74 @@ const getAllNotes = asyncHandler(async (req, res) => {
 // @desc Create new note
 // @route POST /notes
 // @access Private
-const createNewNote = asyncHandler(async (req, res) => {});
+const createNewNote = asyncHandler(async (req, res) => {
+  const { user, title, text } = req.body;
+
+  // Confirm data
+  if (!user || !title || !text) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  // Create and store new note
+  const noteObject = { user, title, text };
+  const note = await Note.create(noteObject);
+
+  if (note) {
+    // created
+    res.status(201).json({ message: `New memo ${title} created` });
+  } else {
+    res.status(400).json({ message: "Invalid user data received" });
+  }
+});
 
 // @desc Update a note
 // @route PATCH /notes
 // @access Private
-const updateNote = asyncHandler(async (req, res) => {});
+const updateNote = asyncHandler(async (req, res) => {
+  const { id, title, text, completed } = req.body;
+
+  //Confirm data
+  if (!id || !title || !text || typeof completed !== "boolean") {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const note = await Note.findById(id).exec();
+
+  if (!note) {
+    return res.status(400).json({ message: "Note not found" });
+  }
+
+  note.title = title;
+  note.text = text;
+  note.completed = completed;
+
+  const updatedNote = await note.save();
+
+  res.json({ message: `${updatedNote.title} updated` });
+});
 
 // @desc Delete a note
 // @route DELETE /notes
 // @access Private
-const deleteNote = asyncHandler(async (req, res) => {});
+const deleteNote = asyncHandler(async (req, res) => {
+  const { _id } = req.body;
+
+  if (!_id) {
+    return res.status(400).json({ message: "Note ID required" });
+  }
+
+  const note = await Note.fineById(_id).exec();
+
+  if (!note) {
+    return res.status(400).json({ message: "Note not found" });
+  }
+
+  const result = await note.deleteOne();
+
+  const reply = `Note ${result.title} deleted`;
+
+  res.json(reply);
+});
 
 module.exports = {
   getAllNotes,
